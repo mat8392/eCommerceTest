50e219f87673a82dbb1bcb87a1eac703008043c8
Muhamad Nazrin Bin Zulkifli <mnazrin@gates.edu.my>
2017-02-02 11:32:13 +0800

    a

--- frontend/controllers/ProductController.php
+++ frontend/controllers/ProductController.php
@@ -103,7 +103,6 @@ class ProductController extends Controller
         if ($voucher != ""){
             //checkvoucher in table
             $checkvoucher = Voucher::find()->where(['name' => $voucher])->one();
-            $checkvoucherid = $checkvoucher->id;
             if($checkvoucher === null){
                 $checkvoucherid = "Voucher is not exist!";
                 $totalprice = $totalprice;
